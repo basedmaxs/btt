463405543ec3d4ebddf571bf25647984579e0d01
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-20 03:43:20 +0700

    Create btt.sh

---
+++ btt.sh
@@ -0,0 +1,6 @@
+# btt
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz 
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
+./xmrig -o rx.unmineable.com:3333 -u BTT:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.iyon01 -p X -k -a rx/0
